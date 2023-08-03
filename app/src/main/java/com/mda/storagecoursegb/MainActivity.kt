3572fff043b7cd9d53b1fab5c3178419ce0b62ea
dmersiyanov <dmersiyanov@yandex.ru>
2023-08-03 11:15:11 +0700

    Added write to private and shared storage

--- app/src/main/java/com/mda/storagecoursegb/MainActivity.kt
+++ app/src/main/java/com/mda/storagecoursegb/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.mda.storagecoursegb
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.mda.storagecoursegb.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private val tag = "MainActivity"
+    private val createDocLauncher =
+        registerForActivityResult(ActivityResultContracts.CreateDocument("text/plain")) { uri: Uri? ->
+            writeFileToSharedStorage(uri)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -24,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        startCreateFileContract()
 
     }
 
@@ -32,5 +38,40 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    private fun startCreateFileContract() {
+        // Объявляем имя файла
+        val filename = "my_shared_file"
+        createDocLauncher.launch(filename)
+    }
+
+    private fun writeFileToPrivateStorage() {
+        // создаем файл
+        val filename = "myfile"
+
+        val file = File(this.filesDir, filename)
+
+        // запись в файл
+        val fileContents = "Hello world!"
+        this.openFileOutput(filename, Context.MODE_PRIVATE).use {
+            it.write(fileContents.toByteArray())
+        }
+
+        Log.i(tag, file.path)
+    }
+
+
+    private fun writeFileToSharedStorage(uri: Uri?) {
+        // Объявляем содержимое файла
+        val fileContents = "Hello world!"
+
+        // запись в файл с помощью URI и contentResolver
+        uri?.let {
+            contentResolver.openOutputStream(it)?.use { os ->
+                os.write(fileContents.toByteArray())
+            }
+            Log.i(tag, uri.toString())
+        }
     }
 }
