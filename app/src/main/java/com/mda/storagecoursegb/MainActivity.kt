cfdca620ff291a3bbe60b25849f669607846e7d2
dmersiyanov <dmersiyanov@yandex.ru>
2023-08-07 12:27:57 +0700

    Added image select and load

--- app/src/main/java/com/mda/storagecoursegb/MainActivity.kt
+++ app/src/main/java/com/mda/storagecoursegb/MainActivity.kt
@@ -1,21 +1,21 @@
 package com.mda.storagecoursegb
 
+import android.net.Uri
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.mda.storagecoursegb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private val pickMediaLauncher =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            loadImage(uri)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -24,13 +24,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-
+        binding.btnSelect.setOnClickListener {
+            showImagePicker()
+        }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    private fun showImagePicker() {
+        pickMediaLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private fun loadImage(uri: Uri?) {
+        if (uri != null) {
+            Log.d("PhotoPicker", "Selected URI: $uri")
+            binding.ivImage.setImageURI(uri)
+        } else {
+            Log.d("PhotoPicker", "No media selected")
+        }
     }
 }
