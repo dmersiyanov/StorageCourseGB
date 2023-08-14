a7da9ebdfc6bc7ef4f024882860c54f1cb16f012
dmersiyanov <dmersiyanov@yandex.ru>
2023-08-14 18:09:58 +0700

    Add buttons

--- app/src/main/java/com/mda/storagecoursegb/MainActivity.kt
+++ app/src/main/java/com/mda/storagecoursegb/MainActivity.kt
@@ -1,30 +1,19 @@
 package com.mda.storagecoursegb
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.documentfile.provider.DocumentFile
+import androidx.core.widget.doAfterTextChanged
 import com.mda.storagecoursegb.databinding.ActivityMainBinding
 import java.io.File
-import java.nio.charset.Charset
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val tag = "MainActivity"
-    private val createDocLauncher =
-        registerForActivityResult(ActivityResultContracts.CreateDocument("text/plain")) { uri: Uri? ->
-            writeFileToSharedStorage(uri)
-        }
-
-    private val openDocLauncher =
-        registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri: Uri? ->
-            readFileFromUri(uri)
-        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -33,59 +22,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnCreateDoc.setOnClickListener {
-            launchCreateFileContract()
+        binding.etFileName.doAfterTextChanged {
+            binding.btnSave.isEnabled =
+                !it.isNullOrEmpty() && !binding.etFileContent.text.isNullOrEmpty()
         }
 
-        binding.btnOpenDoc.setOnClickListener {
-            openDocLauncher.launch(arrayOf("text/plain"))
+        binding.etFileContent.doAfterTextChanged {
+            binding.btnSave.isEnabled =
+                !it.isNullOrEmpty() && !binding.etFileName.text.isNullOrEmpty()
+        }
+
+        binding.btnSave.setOnClickListener {
+            writeFileToPrivateStorage(
+                binding.etFileName.text.toString(),
+                binding.etFileContent.text.toString()
+            )
         }
     }
 
-    private fun launchCreateFileContract() {
-        // Объявляем имя файла
-        val filename = "my_shared_file"
-        createDocLauncher.launch(filename)
-    }
-
-    private fun writeFileToPrivateStorage() {
+    private fun writeFileToPrivateStorage(name: String, content: String) {
         // создаем файл
-        val filename = "myfile"
-
-        val file = File(this.filesDir, filename)
+        val file = File(this.filesDir, name)
 
         // запись в файл
-        val fileContents = "Hello world!"
-        this.openFileOutput(filename, Context.MODE_PRIVATE).use {
-            it.write(fileContents.toByteArray())
+        this.openFileOutput(name, Context.MODE_PRIVATE).use {
+            it.write(content.toByteArray())
         }
 
-        Log.i(tag, file.path)
-    }
-
-
-    private fun writeFileToSharedStorage(uri: Uri?) {
-        // Объявляем содержимое файла
-        val fileContents = "Hello world!"
-
-        // запись в файл с помощью URI и contentResolver
-        uri?.let {
-            contentResolver.openOutputStream(it)?.use { os ->
-                os.write(fileContents.toByteArray())
-            }
-            Log.i(tag, uri.toString())
-        }
-    }
-
-    private fun readFileFromUri(uri: Uri?) {
-        uri?.let {
-            val documentFile = DocumentFile.fromSingleUri(this, it)
-            if (documentFile?.canRead() == true) {
-                contentResolver.openInputStream(documentFile.uri)?.use { ips ->
-                    val content = ips.readBytes().toString(Charset.defaultCharset())
-                    Log.i(tag, content)
-                }
-            }
+        if (file.exists()) {
+            // Вывод путь текущего файла в Logcat
+            Log.i(tag, file.path)
         }
     }
 }
